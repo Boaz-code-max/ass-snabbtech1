903cbdd4ae4515bddc94b56e4e3f698e097ff9c0
boazparakati <boazparakati92@gmail.com>
2024-01-11 21:21:13 +0530

    ass-snabbtech

---
+++ ass-snabbtech/prct.tf
@@ -0,0 +1,103 @@
+provider "aws" {
+  region = "eu-north-1"
+  
+}
+
+resource "aws_instance" "demo-server" {
+  ami           = "ami-0014ce3e52359afbd"
+  instance_type = "t2.micro"
+  key_name = "Boazp1"
+  ##security_groups = ["demo-secgroup"]
+  vpc_security_group_ids = [ aws_security_group.demo-secgroup.id ]
+  subnet_id = aws_subnet.chinnusubnets-01.id
+for_each = toset( ["Development", "Production", ] )
+  tags = {
+    Name = "${each.key}"
+  }
+}
+
+resource "aws_security_group" "demo-secgroup" {
+  name        = "demo-secgroup"
+  description = "ssh access"
+  vpc_id = aws_vpc.chanduvpc.id 
+  
+  ingress {
+    description      = "ssh access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    description      = "jenkins port"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "b-sg"
+  }
+}
+resource "aws_vpc" "chanduvpc" {
+  cidr_block = "10.1.0.0/16"
+  tags = {
+    Name = "chanduvpc"
+  }
+}
+
+resource "aws_subnet" "chinnusubnets-01" {
+  vpc_id     = aws_vpc.chanduvpc.id
+  cidr_block = "10.1.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "eu-north-1b"
+  tags = {
+    Name = "chinnusubnets-01"
+  }
+  depends_on = [ aws_vpc.chanduvpc ]
+}
+resource "aws_subnet" "chinnusubnets-02" {
+  vpc_id     = aws_vpc.chanduvpc.id
+  cidr_block = "10.1.2.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "eu-north-1a"
+  tags = {
+    Name = "chinnusubnets-02"
+  }
+}
+resource "aws_internet_gateway" "chandu-igw" {
+  vpc_id = aws_vpc.chanduvpc.id
+  tags = {
+    Name = "chandu-igw"
+  }
+  
+}
+resource "aws_route_table" "chandu-rt" {
+  vpc_id = aws_vpc.chanduvpc.id
+  route  {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.chandu-igw.id 
+  }
+  
+}
+
+resource "aws_route_table_association" "chandu-rta-1" {
+  subnet_id = aws_subnet.chinnusubnets-01.id
+  route_table_id = aws_route_table.chandu-rt.id 
+  
+}
+resource "aws_route_table_association" "chandu-rta-2" {
+  subnet_id = aws_subnet.chinnusubnets-02.id
+  route_table_id = aws_route_table.chandu-rt.id
+  
+}
